07832525a9a5a87b3a0be3f5f0b02ce639cfa170
Mara Lampert <mara.lampert@mailbox.tu-dresden.de>
2024-07-23 17:50:21 +0200

    modify _clusterplot.py to allow histogram #56

--- stackview/_clusterplot.py
+++ stackview/_clusterplot.py
@@ -2,6 +2,7 @@ def clusterplot(df, labels, column_x:str="x", column_y:str="y", column_selection
                      zoom_factor:float=1, zoom_spline_order:int=0, figsize=(4, 4), alpha:float=0.5):
     """
     Visualizes a scatter plot of columns in a given dataframe next to a segmented image.
+    If one chooses the same column in x and y, a histogram is plotted instead.
 
     Inspired by the napari clusters plotter:
     https://github.com/BiAPoL/napari-clusters-plotter
@@ -38,12 +39,15 @@ def clusterplot(df, labels, column_x:str="x", column_y:str="y", column_selection
     from ._curtain import curtain
     from ._slice import slice
     from ._scatterplot import scatterplot
+    from ._histogram import histogram 
     import functools
 
+    # check if the selection column is in the dataframe
     if column_selection in df.columns:
         selection = df["selection"].tolist()
 
         selected_image = np.take(np.asarray([-1] + list(selection)) * 1 + 1, labels).astype(np.uint32)
+    # if not, use the labels
     else:
         selected_image = ((labels > 0) * 1).astype(np.uint32)
 
@@ -52,6 +56,7 @@ def clusterplot(df, labels, column_x:str="x", column_y:str="y", column_selection
     else:
         image_display = curtain(image, selected_image, zoom_factor=zoom_factor, zoom_spline_order=zoom_spline_order, alpha=alpha)
 
+    # function to update the selection
     def update(selection, label_image, selected_image, widget):
 
         temp = np.take(np.asarray([-1] + list(selection)) * 1 + 1, label_image)
@@ -62,14 +67,23 @@ def clusterplot(df, labels, column_x:str="x", column_y:str="y", column_selection
         # redraw the visualization
         widget.update()
 
+    # partial function to update the selection
     update_selection = functools.partial(update, label_image=labels, selected_image=selected_image,
                                          widget=image_display)
 
-    scatterplot = scatterplot(df, column_x, column_y, column_selection, figsize=figsize,
+    # plotting:
+    # If one chooses different columns in x and y, a scatter plot is plotted 
+    if column_x != column_y:
+        plot = scatterplot(df, column_x, column_y, column_selection, figsize=figsize,
                                         selection_changed_callback=update_selection)
 
+    # If one chooses the same column in x and y, a histogram is plotted
+    else:
+        plot = histogram(df, column_x, column_selection, figsize=figsize,
+                                         selection_changed_callback=update_selection)
+        
+    # return the grid
     return grid([[
         image_display,
-        scatterplot
+        plot
     ]])
-
